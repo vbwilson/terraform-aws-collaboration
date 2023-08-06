5bdcecf340c5c3faa70d99bc7514ff5a295f39a8
vbwilson <sangrok.jeong@medicalai.com>
2023-08-06 20:55:46 +0900

    tfc state

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 terraform {
-  // cloud {
-  //   organization = "<MY_ORG_NAME>"         # 생성한 ORG 이름 지정
-  //   hostname     = "app.terraform.io"      # default
+   cloud {
+     organization = "vbwilson-org"         # 생성한 ORG 이름 지정
+     hostname     = "app.terraform.io"      # default
 
-  //   workspaces {
-  //     name = "collaboration"  # 없으면 생성됨
-  //   }
-  // }
+     workspaces {
+       name = "terraform-aws-collaboration"  # 없으면 생성됨
+     }
+   }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
